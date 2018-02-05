c4989deb7c8bf28bb07cc88ee0862161804c2e9c
karengl <kglee@ucsc.edu>
2018-02-04 20:38:17 -0800

    added save recipe button

--- src/app/utils/recipe.resolver.ts
+++ src/app/utils/recipe.resolver.ts
@@ -30,7 +30,7 @@ export class RecipeResolver implements Resolve<Recipe> {
         },
         {
           amount: '1',
-          text: 'slice of cheese'
+          text: 'cup of cheese'
         },
         {
           amount: '',
@@ -38,7 +38,7 @@ export class RecipeResolver implements Resolve<Recipe> {
         }
       ],
       directions: [
-        'Prep',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         'Cook',
         'Eat'
       ],    

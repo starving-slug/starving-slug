199aa1f79e42793511979d2c7bb48d479c1120ec
Danny Boyd <dacboyd@ucsc.edu>
2018-02-24 20:03:45 -0800

    Added requirement on secrets file, added signin to the header nav bar instead of a separate page

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -6,21 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  private myClientId: string = '163621476624-55cuuke78c1krolj8a5fndp6es9nvopt.apps.googleusercontent.com';
+  private myClientId: string = '';
 
   constructor() { }
 
   ngOnInit() {
   }
-
-  onGoogleSignInSuccess(event: GoogleSignInSuccess) {
-    let googleUser: gapi.auth2.GoogleUser = event.googleUser;
-    let id: string = googleUser.getId();
-    let profile: gapi.auth2.BasicProfile = googleUser.getBasicProfile();
-    console.log('ID: ' +
-      profile
-        .getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-  }
-
+  
 }

962c13d88d9c28422ae4af0a9cbd8138afddbdb0
jrammmy <justinramiscal@gmail.com>
2018-03-02 01:20:26 -0800

    search-page component can now use functions from the apiService

--- src/app/components/pages/search-page/search-page.component.ts
+++ src/app/components/pages/search-page/search-page.component.ts
@@ -1,24 +1,28 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm  } from '@angular/forms';
+import { ApiService } from '../../../utils/apiService'
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
   styleUrls: ['./search-page.component.css']
 })
+
+
 export class SearchPageComponent implements OnInit {
 @ViewChild('f') searchFilter: NgForm;
-  constructor() { }
+
+  constructor(private api: ApiService) { }
 
   ngOnInit() {
+
   }
 
-  // onSubmit(form: NgForm){
-  //     console.log(form);
-  // }
 
   onSubmit(){
-      console.log(this.searchFilter);
+      // console.log(this.searchFilter);
+      // console.log(this.searchFilter.value);
+      console.log(this.api.getUser("shashank_oddessey"))
   }
 
 }

8d5a1f0b5ff25c0b0b0f067e7553d5ef5dec41e4
Danny Boyd <danny.boyd@productops.com>
2018-03-11 13:37:36 -0700

    Google signin 2 (#10)
    * Pulls user data from the api to populate profile pages
    
    * forgot to include implementation file
    
    * Slapped the google signin stuff into the front end
    
    * Added requirement on secrets file, added signin to the header nav bar instead of a separate page
    
    * fixed require error by adding types: [
      node
    ] under src/tsconfig.app.json
    
    * Google signin runs, calls signin function that logs only
    
    * sprint 3 report and sprint 4 plan added
    
    * created session service
    
    * Updated header to only display data
    updated session service to entirely handle the session data
    listener for session updates lives in the app component, may need to move again, issue was service-level
    
    * prints idtoken to the screen
    
    * User signin for the first time redirects to a create profile page, currently only logs the updated profile data
    
    * Framework for api call

--- src/app/components/nav/header/header.component.ts
+++ src/app/components/nav/header/header.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormControl, NgForm } from '@angular/forms';
 import { GoogleSignInSuccess } from 'angular-google-signin';
+// import { environment } from '../../../../environments/environment';
+
+import { SessionService } from '../../../utils';
 import { environment } from '../../../../environments/environment';
 import { Router } from '@angular/router';
 import { ApiService } from '../../../utils/apiService';
@@ -12,29 +15,74 @@ import { Recipe } from '../../../models/recipe.model'
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent implements OnInit {
-  testlogin: boolean = false;
+
   private myClientId = environment.GClientId;
+  private _user = null;
+
+  @Input() set user(user) {
+    console.log(user);
+    this._user = user;
+  };
+
+  get user() {
+    return this._user;
+  };
+
+  get activeSession() {
+    console.log(this._user !== null);
+    return this._user !== null;
+  }
 
   search = '';
 
   constructor(private api: ApiService,
-              private router: Router) {
+              private router: Router,
+              private session: SessionService) {
        console.log(environment)
   }
 
   ngOnInit() {
   }
 
-  onGoogleSignInSuccess(event: GoogleSignInSuccess) {
-    let googleUser: gapi.auth2.GoogleUser = event.googleUser;
-    let id: string = googleUser.getId();
-    let profile: gapi.auth2.BasicProfile = googleUser.getBasicProfile();
-    console.log('ID: ' +
-      profile
-        .getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
+  // signIn(event: GoogleSignInSuccess) {
+  //   let googleUser: gapi.auth2.GoogleUser = event.googleUser;
+  //   let id: string = googleUser.getId();
+  //   let id_token = googleUser.getAuthResponse().id_token;
+  //
+  //   let profile: gapi.auth2.BasicProfile = googleUser.getBasicProfile();
+  //   console.log(profile);
+  //   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  //   console.log('Name: ' + profile.getName());
+  //   this.api.signIn(profile.getName(), id_token);
+  // }
+
+  signIn(event: any) {
+    this.session.signIn(event)
   }
+
+  signOut() {
+    console.log('Sign out');
+    this.session.signOut();
+  }
+
+  // onGoogleSignInSuccess(event: GoogleSignInSuccess) {
+  //   let googleUser: gapi.auth2.GoogleUser = event.googleUser;
+  //   let id: string = googleUser.getId();
+  //   let profile: gapi.auth2.BasicProfile = googleUser.getBasicProfile();
+  //   console.log('ID: ' +
+  //     profile
+  //       .getId()); // Do not send to your backend! Use an ID token instead.
+  //   console.log('Name: ' + profile.getName());
+  // }
+  //
+  // signOut() {
+  //   let auth2 = gapi.auth2.getAuthInstance();
+  //   auth2.signOut().then(() => {
+  //     console.log('Signed out');
+  //   })
+  // }
 
   onSubmit(form: NgForm) {
       this.search = JSON.stringify(form.value);
---
+++ src/app/utils/sessionService.ts
@@ -0,0 +1,67 @@
+import { Injectable, NgZone } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { GoogleSignInSuccess } from 'angular-google-signin';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+import { ApiService } from './apiService';
+
+@Injectable()
+export class SessionService {
+
+  private _signedIn: boolean;
+  signedIn$: BehaviorSubject<any>;
+  googleUser$: BehaviorSubject<any>;
+
+  constructor(private api: ApiService, private ngZone: NgZone, private router: Router, private route: ActivatedRoute) {
+    this.signedIn$ = new BehaviorSubject(null);
+    this.googleUser$ = new BehaviorSubject(null);
+  }
+
+  signIn(event: GoogleSignInSuccess) {
+    console.log('Signing In');
+    let googleUser: gapi.auth2.GoogleUser = event.googleUser;
+    let id: string = googleUser.getId();
+    let profile: gapi.auth2.BasicProfile = googleUser.getBasicProfile();
+    let id_token = googleUser.getAuthResponse().id_token;
+
+    this.api.signIn(profile, id_token).subscribe((res) => {
+      console.log(res);
+      if (res['newLogin']) {
+        console.log(this.route.snapshot);
+        let prevRoute = this.route.snapshot;
+        console.log('This is a first-time login!');
+        this.router.navigate(['./signup']);
+
+        // save the current url
+        // redirect to the signup page, pass the old url as a query or something
+      }
+      let session = {
+        username: profile.getName(),
+        image: profile.getImageUrl(),
+      }
+      this.ngZone.run(() => {
+        this.googleUser$.next(profile);
+        this.signedIn$.next(session);
+      });
+    }, (error) => {
+      console.log(error.message);
+    });
+
+    // console.log('ID: ' +
+    //   profile
+    //     .getId()); // Do not send to your backend! Use an ID token instead.
+    // console.log('Name: ' + profile.getName());
+  }
+
+  signOut() {
+    console.log('Clicked sign out button')
+    let auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(() => {
+      console.log('Signed out');
+      this.ngZone.run(() => {
+        this.googleUser$.next(null);
+        this.signedIn$.next(null);
+      });
+    })
+  }
+}

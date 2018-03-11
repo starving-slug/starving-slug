16b14532e107592980cd702e9ecabffe8dddf482
Tianyi Zhang <tzhang24@ucsc.edu>
2018-03-11 14:40:09 -0700

    submit not working yet

--- src/app/components/pages/profile-page/profile-page.component.ts
+++ src/app/components/pages/profile-page/profile-page.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NgForm  } from '@angular/forms';
+import { FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { User } from '../../../models'
 import{ ApiService } from '../../../utils/apiService'
+
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
@@ -11,13 +15,23 @@ import{ ApiService } from '../../../utils/apiService'
 export class ProfilePageComponent implements OnInit {
   user: User;
 
-  constructor(private route: ActivatedRoute) { // this is all currently filler junk
+  constructor(private route: ActivatedRoute, private api: ApiService) { // this is all currently filler junk
     console.log(route.snapshot.data)
     this.user = route.snapshot.data['user'];
     console.log(this.user);
   }
-
   ngOnInit() {
   }
 
+  onSubmit(commentForm: NgForm) {
+    let body = commentForm.value;
+    this.api.createComment(body).subscribe((res) => {
+        console.log("Success!");
+    }, (err) => {
+      console.log("Error");
+      console.error(err.message);
+    });
 }
+
+}
+

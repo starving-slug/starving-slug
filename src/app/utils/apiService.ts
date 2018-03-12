65b02b5ac0300b30908e35b759b41ee0717b33a4
Danny Boyd <dacboyd@ucsc.edu>
2018-03-12 14:11:44 -0700

    Profiles can be created

--- src/app/utils/apiService.ts
+++ src/app/utils/apiService.ts
@@ -8,67 +8,16 @@ let apiurl = 'http://localhost:3000'
 
 @Injectable()
 export class ApiService {
-<<<<<<< Updated upstream
-  constructor(private http: HttpClient,
-              private storage: StorageService) {
-  }
-
-  signIn(profile: gapi.auth2.BasicProfile, id_token: string) {
-    console.log(profile, id_token);
-    let body = {
-      id_token: id_token,
-      username: profile.getName(),
-      // image: profile.given_name
+    constructor(private http: HttpClient,
+                private storage: StorageService) {
     }
-    return this.http.post(`${apiurl}/user`, body);
-  }
-
-  updateProfile(profile: Object, permissions: string) {
-    console.log(profile, permissions);
-    return this.http.post(`${apiurl}/profile`, profile, {headers: {authorization: `Bearer ${permissions}`}});
-  }
-
-  getUser(username: string): Observable<any> {
-    console.log(`Fetching user ${username}`)
-    return this.http.get(`${apiurl}/profile/${username}`);
-  }
-
-  getRecipe(id: string): Observable<any> {
-    console.log(`Fetching recipe ${name}`);
-    return this.http.get(`${apiurl}/recipe/${id}`);
-  }
-
-  createRecipe(res: Object[]): Observable<any> {
-    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    return this.http.post(`${apiurl}/recipe`, res, { headers: headers, responseType: 'text' });
-  }
-
-  getSearch(name: string): Observable<any> {
-    console.log(`Fetching recipe ${name}`);
-    return this.http.get(`${apiurl}/search?name=${name}`);
-  }
-
-  getFilter(name: string, author: string, tag: string): Observable<any> {
-    console.log(`Fetching recipe ${name}`);
-    return this.http.get(`${apiurl}/search?name=${name}&author=${author}&tag=${tag}`);
-  }
-
-  deleteRecipe(id: string): Observable<any> {
-    console.log(id);
-    return this.http.delete(`${apiurl}/recipe/delete/${id}`);
-  }
-
-  createComment(res: Object[]): Observable<any> {
-    const headers = new HttpHeaders({'Content-Type': 'application/json'});
-    console.log(res);
-    return this.http.patch(`${apiurl}/profile-comment/${res[0]['author']}`, res, {headers: headers, responseType: 'text'});
-  }
-=======
 
     signIn(profile: gapi.auth2.BasicProfile, id_token: string) {
       console.log(profile, id_token);
       let body = {
-        id_token: id_token
+        id_token: id_token,
+        username: profile.getName(),
+        // image: profile.given_name
       }
       return this.http.post(`${apiurl}/user`, body);
     }
@@ -106,5 +55,4 @@ export class ApiService {
         return this.http.get(`${apiurl}/search?name=${name}&author=${author}&tag=${tag}`);
     }
 
->>>>>>> Stashed changes
 }
